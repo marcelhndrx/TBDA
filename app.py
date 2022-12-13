a1014ad58a23c53843669745843edb2ca937e189
marcelhndrx <64195946+marcelhndrx@users.noreply.github.com>
2022-12-13 02:10:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,42 @@
-from datetime import datetime,date,time,timezone,timedelta
-import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly
-import plotly.graph_objects as go
 import plotly.express as px
-from matplotlib import pyplot as plt
+import datetime
+import time
+import matplotlib.pyplot as plt
+import copy
+import os
 from sqlalchemy import create_engine #to access a sql database
 
 engine = create_engine('postgresql://lectura:ncorrea#2022@138.100.82.178:5432/2207')
-
-# Recuperation of all operating modes
-OPmodes = pd.read_sql_query("select id_var, value from variable_log_float where id_var=622 limit 1000", con=engine)
-list1=OPmodes["value"]
-x= np.array(list1)
-op_modes = np.unique(x).astype(str)[:-1]
-def query(x):
-    return pd.read_sql_query("".join(["select id_var, date, to_timestamp(@date/1000) as dateH, value from variable_log_float where id_var=622 and value=",x]),con=engine)
-modes = [query(x) for x in op_modes]
-
-st.title("Operating periods")
-
-# Chose time window
-
-start_date = st.date_input("Start date",date(2020,12,28),date(2020,12,28),date(2022,2,23))
-start_time = st.time_input("Start time",time(6,00,00))
-end_date = st.date_input("End date",date(2020,12,28),date(2020,12,28),date(2022,2,23))
-end_time = st.time_input("End time",time(22,00,00))
-
-start_datetime = datetime.combine(start_date,start_time,tzinfo=timezone(timedelta(seconds=3600)))
-end_datetime = datetime.combine(end_date,end_time,tzinfo=timezone(timedelta(seconds=3600)))
-
-#Number of minutes in the interval
-time_interval = end_datetime - start_datetime
-time_interval_min = time_interval.seconds/60
+df = pd.read_sql_query("SELECT id_var, date, value FROM variable_log_float WHERE id_var = 575 LIMIT 3000", con=engine)
+# Convert the values in the "date" column to datetime objects
+df["date"] = pd.to_datetime(df["date"], unit="ms")
 
 
-# operations collected and operations per minute
-counters_collected = []
-counters_per_min = []
+# Shift the "value" column by one row and store it in a new column called "prev_value"
+# Use the fill_value parameter to specify a default value to use for the shifted rows
+df["prev_value"] = df["value"].shift(fill_value=df["value"].iloc[0])
+
+# Create an empty list to store the data for the Gantt chart
+data = []
+
+# Select the rows where the "value" changes from 255 to 0 or vice versa
+rows = df.where((df["value"] == 0) & (df["prev_value"] == 255) | (df["value"] == 255) & (df["prev_value"] == 0))
+
+#Iterate through the selected rows and create the data for the Gantt chart
+for index, row in rows.iterrows():
+    task = "on" if row["value"] == 255 else "off"
+    start = row["date"]
+    finish = df["date"].shift(-1).iloc[index]
+    data.append({"task": task, "start": start, "finish": finish})
 
 
-for mode in modes:
-    op_collected = mode[mode['dateh'] > start_datetime][mode['dateh'] < end_datetime]['dateh'].size
-    counters_collected.append(op_collected)
-    counters_per_min.append(op_collected/time_interval_min)
+# Create the Gantt chart using the data
+fig = px.timeline(data, x_start="start", x_end="finish", y="task")
 
-st.text("Operating periods for chosen time window")
-
-df = pd.DataFrame([
-    dict(Task="Operating, on/off", Start=start_datetime, Finish=end_datetime),
-])
-
-fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task")
-fig.update_yaxes(autorange="reversed") # otherwise tasks are listed from the bottom up
-st.plotly_chart(fig, use_container_width=True)
+# Show the Gantt chart
+fig.show()
 
 
     
