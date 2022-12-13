b6740d488be8dda8b96236831efd0d409c117fac
marcelhndrx <64195946+marcelhndrx@users.noreply.github.com>
2022-12-13 13:01:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 from sqlalchemy import create_engine #to access a sql database
 
 engine = create_engine('postgresql://lectura:ncorrea#2022@138.100.82.178:5432/2207')
-df = pd.read_sql_query("SELECT id_var, date, value FROM variable_log_float WHERE id_var = 575 LIMIT 500", con=engine)
+df = pd.read_sql_query("SELECT id_var, date, value FROM variable_log_float WHERE id_var = 575 LIMIT 800", con=engine)
 # Convert the values in the "date" column to datetime objects
 df["date"] = pd.to_datetime(df["date"], unit="ms")
 
@@ -27,18 +27,19 @@ rows = df.where((df["value"] == 0) & (df["prev_value"] == 255) | (df["value"] ==
 
 #Iterate through the selected rows and create the data for the Gantt chart
 for index, row in rows.iterrows():
-    task = "on" if row["value"] == 255 else "off"
+    status = "on" if row["value"] == 255 else "off"
     start = row["date"]
     finish = df["date"].shift(-1).iloc[index]
-    data.append({"task": task, "start": start, "finish": finish})
+    data.append({"status": status, "start": start, "finish": finish})
 
 
 # Create the Gantt chart using the data
-fig = px.timeline(data, x_start="start", x_end="finish", y="task")
-#fig = px.timeline(data, x_start="start", x_end="finish", y="task", bar_width=0.2)
+fig = px.timeline(data, x_start="start", x_end="finish", y="status")
 
-# # Show the Gantt chart
-# fig.show()                    #FORVISUALSTUDIOCODE
+
+#Show the Gantt chart
+
+#fig.show()                    #FORVISUALSTUDIOCODE
 
 st.plotly_chart(fig)
 
